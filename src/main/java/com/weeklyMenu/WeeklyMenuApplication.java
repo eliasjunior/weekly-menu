a3727914bd2c6d1b68d825f5c7a96398735ef309
eliasjunior <emcjunior@gmail.com>
2021-02-10 22:01:00 +0000

    test CI

--- src/main/java/com/weeklyMenu/WeeklyMenuApplication.java
+++ src/main/java/com/weeklyMenu/WeeklyMenuApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class WeeklyMenuApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WeeklyMenuApplication.class, args);
     }
-
-
 }
