f220a9e9805b285b44ac4e4095d268601d8ab9ee
eliasjunior <emcjunior@gmail.com>
2022-01-16 20:36:53 +0100

    remove hosts from code

--- springApp/src/main/java/com/weeklyMenu/adaptor/config/BaseConfig.java
+++ springApp/src/main/java/com/weeklyMenu/adaptor/config/BaseConfig.java
@@ -4,6 +4,7 @@ import com.weeklyMenu.adaptor.mapper.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -13,12 +14,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BaseConfig {
+    @Value("${cors.allowed_host}")
+    private String [] hosts;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                final String [] HOSTS_ALLOWED = {"http://localhost:3001", "https://weekly-menu-ui.herokuapp.com"};
+                final String [] HOSTS_ALLOWED = hosts;
                 registry.addMapping("/**")
                         .allowedMethods("*")
                         .allowedOrigins(HOSTS_ALLOWED);
